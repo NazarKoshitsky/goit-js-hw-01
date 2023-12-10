591869760e8c5dfa9f9875a51e0171dc7178a11a
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2023-12-10 22:01:53 +0100

    2

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,4 @@
 "use strict"
-
 let quantity;
 let pricePerDroid;
 function makeTransaction(quantity, pricePerDroid) {
@@ -8,7 +7,7 @@ function makeTransaction(quantity, pricePerDroid) {
 }
 
 makeTransaction(quantity, pricePerDroid) 
-    console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-    console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-    console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
 
